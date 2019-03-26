55a71065893e1b39702d5ef44a3e3fd16655c028
ils <kubasehxc@gmail.com>
2019-03-27 00:49:55 +0100

    feat:  - Build card layout  - Add material style with custom color scheme

--- src/components/ingredients/ingredient/ingredient.js
+++ src/components/ingredients/ingredient/ingredient.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+import './ingredient.scss';
+
 export function Ingredient({ ingredient, onClick }) {
-  return (
-    <div className="pw-ingredient" onClick={() => onClick(ingredient)}>
-      {ingredient.name}
-    </div>
-  );
+    return (
+        <div className='pw-ingredient mdl-card mdl-shadow--2dp'>
+            <div className='pw-ingredient__title mdl-card__title'>
+                <div className='mdl-card__title-text'>{ingredient.name}</div>
+            </div>
+            <div className='pw-ingredient__body mdl-card__supporting-text'>
+                {ingredient.description}
+            </div>
+            <div className='pw-ingredient__footer mdl-card__actions'>
+                {ingredient.type === 'simple' && ingredient.multiplicity > 1 ? (
+                    <React.Fragment>
+                        <button
+                            onClick={() => onClick(ingredient, 'pop')}
+                            className='pw-ingredient__button pop mdl-button mdl-js-button mdl-button--primary'
+                        >
+                            -
+                        </button>
+                        <div>{`0/${ingredient.multiplicity}`}</div>
+                        <button
+                            onClick={() => onClick(ingredient, 'add')}
+                            className='pw-ingredient__button add mdl-button mdl-js-button mdl-button--accent'
+                        >
+                            +
+                        </button>
+                    </React.Fragment>
+                ) : (
+                    <button
+                        onClick={() => onClick(ingredient, 'single')}
+                        className='mdl-button mdl-js-button mdl-button--accent'
+                    >
+                        Seleccionar
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
